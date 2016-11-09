a85d07647f648ebc872c8188a76108cf439280e6
Thomas T. Thai <thomas.t.thai@gmail.com>
2016-11-09 03:26:45 -0800

    Made buttons larger and full width. Changed add description to About tab.

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-
 import { HomePage } from '../home/home';
-import { UploadingPage } from '../uploading/uploading';
 import { AboutPage } from '../about/about';
 
 @Component({
